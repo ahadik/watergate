1884b787253550e527ff0a27ccf7c2f57a757a04
Steven Raden <stevengraden@gmail.com>
2016-02-20 15:43:55 -0600

    Not seeing it connect to Mongo. Setup useage of compose url

---
+++ webapp/index.js
@@ -0,0 +1,24 @@
+/* global __dirname */
+var express = require("express"),
+    app = express(),
+    mongoose = require('mongoose');
+
+var mURL = "";
+//if dev
+if (process.env.ENVIRONMENT == "dev") {
+  mURL = 'mongodb://' + process.env.MONGO_USERNAME + ':' + process.env.MONG_PASS + '@aws-us-east-1-portal.10.dblayer.com:10576/watergatedb-dev';
+}
+mongoose.connect('mURL');
+var db = mongoose.connection; //db = mongoose connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected');
+});
+
+app.get("/", function (req, res) {
+	res.send("hey"); //Send back html file
+});
+
+// app.listen(8000, function () {
+// 	console.info('Server listening on port ' + this.address().port);
+// });
